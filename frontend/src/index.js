4628178cf415ce824f1c2a8f7f1b53e6e1a44c73
bevziuk <1bs14b.bevziuk@gmail.com>
2018-03-26 18:31:04 +0300

    prebank

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -2,5 +2,6 @@ import './promise-polyfill'
 import 'element-ui/lib/theme-chalk/index.css'
 import './index.scss'
 import { app } from './app'
+require('offline-plugin/runtime').install();
 
 app.$mount('#app');
